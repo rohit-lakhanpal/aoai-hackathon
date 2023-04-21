12da1055a06aed29b8d765cef584211bb77ee691
Rohit Lakhanpal <rohit-lakhanpal@users.noreply.github.com>
2023-04-21 11:12:36 +1000

    Bootstrap ejs app
    Initial commit bootstrapping the ejs apis for the hackathon.

---
+++ src/javascript_nodejs/api/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+var apiBase = '/api';
+app.use(`${apiBase}/`, require('./routes/index'));
+
+module.exports = app;
